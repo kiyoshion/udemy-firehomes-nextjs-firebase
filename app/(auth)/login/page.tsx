d19bf2a4371fedb23cdf32d67d38e38b063c63c9
adam <adam@adamnoMacBook-Pro.local>
2024-12-11 16:59:11 +0900

    add auth pages

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,19 @@
+import ContinueWithGoogleButton from "@/components/continue-with-google-button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Login() {
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-3xl font-bold">
+            Login
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ContinueWithGoogleButton />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
