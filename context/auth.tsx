2e0de347eb8b56fa675c472c1a77029d74407788
adam <adam@adamnoMacBook-Pro.local>
2024-12-15 23:17:26 +0900

    add context

--- context/auth.tsx
+++ context/auth.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { auth } from "@/firebase/client";
-import { GoogleAuthProvider, signInWithPopup, User } from "firebase/auth";
+import { GoogleAuthProvider, ParsedToken, signInWithPopup, User } from "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
+import { removeToken, setToken } from "./actions";
 
 type AuthContextType = {
   currentUser: User | null;
   logout: () => Promise<void>;
   loginWithGoogle: () => Promise<void>;
+  customClaims: ParsedToken | null;
 }
 
 const AuthContext = createContext<AuthContextType | null>(null);
@@ -16,10 +18,26 @@ export const AuthProvider = ({ children }: {
   children: React.ReactNode
 }) => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [customClaims, setCustomClaims] = useState<ParsedToken | null>(null);
 
   useEffect(() => {
-    const unbsubscribe = auth.onAuthStateChanged((user) => {
+    const unbsubscribe = auth.onAuthStateChanged(async (user) => {
       setCurrentUser(user ?? null);
+      if (user) {
+        const tokenResult = await user.getIdTokenResult();
+        const token = tokenResult.token;
+        const refreshToken = user.refreshToken;
+        const claims = tokenResult.claims;
+        setCustomClaims(claims ?? null);
+        if (token && refreshToken) {
+          await setToken({
+            token,
+            refreshToken
+          });
+        } else {
+          await removeToken();
+        }
+      }
     });
 
     return () => unbsubscribe();
@@ -40,6 +58,7 @@ export const AuthProvider = ({ children }: {
         currentUser,
         logout,
         loginWithGoogle,
+        customClaims
       }}
     >
       {children}
