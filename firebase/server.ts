52bdb044ef181130002d67576bf7aa7523f33b57
adam <adam@adamnoMacBook-Pro.local>
2024-12-15 23:18:53 +0900

    update firebase for server

--- firebase/server.ts
+++ firebase/server.ts
@@ -1,6 +1,7 @@
 import { Firestore, getFirestore } from "firebase-admin/firestore"
 import { getApps, ServiceAccount } from "firebase-admin/app"
 import admin from "firebase-admin";
+import { Auth, getAuth } from "firebase-admin/auth";
 
 const serviceAccount = {
   "type": "service_account",
@@ -17,6 +18,7 @@ const serviceAccount = {
 }
 
 let firestore: Firestore;
+let auth: Auth;
 const currentApps = getApps();
 
 if (!currentApps.length) {
@@ -24,9 +26,23 @@ if (!currentApps.length) {
     credential: admin.credential.cert(serviceAccount as ServiceAccount),
   });
   firestore = getFirestore(app);
+  auth = getAuth(app);
 } else {
   const app = currentApps[0];
   firestore = getFirestore(app);
+  auth = getAuth(app);
 }
 
-export { firestore };
+export { firestore, auth };
+
+export const getTotalPages = async (firestoreQuery: FirebaseFirestore.Query<
+    FirebaseFirestore.DocumentData,
+    FirebaseFirestore.DocumentData
+  >, pageSize: number) => {
+    const queryCount = firestoreQuery.count();
+    const countSnapshot = await queryCount.get();
+    const countData = countSnapshot.data();
+    const total = countData.count;
+    const totalPages = Math.ceil(total / pageSize);
+    return totalPages;
+  }
